3dc0070a696d12f0c338d5c8cf20020086253132
catpleademoji <catpleademoji@gmail.com>
2025-05-20 22:35:31 +0000

    update package.json

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { CatPleadMerge } from "./CatPleadMerge";
+import { Cat } from "./types/Cat";
+import { Particle } from "./types/Particle";
+import { SoundEffect } from "./types/SoundEffect";
+import { Theme } from "./types/Theme";
+
+export {
+    CatPleadMerge,
+};
+
+export type { Cat, Particle, SoundEffect, Theme };
